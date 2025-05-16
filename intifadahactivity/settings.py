a17c5a5c9f789d1dfd5a930723d69666857cd36e
Syaikhul Hadi <my.ant2008gmail.com>
2025-05-16 22:50:18 +0700

    change settings

--- intifadahactivity/settings.py
+++ intifadahactivity/settings.py
@@ -73,7 +73,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
